78e299c7c532e32f0a665b58c7dcc145d72441fd
Omer Erbalta <erbalta80@gmail.com>
2023-11-16 14:02:30 +0300

    Initial Commit

---
+++ FlippablePostCard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlippablePostCard
+//
+//  Created by OmerErbalta on 16.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlippablePostCard/FlippablePostCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlippablePostCardApp.swift
+//  FlippablePostCard
+//
+//  Created by OmerErbalta on 16.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlippablePostCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
